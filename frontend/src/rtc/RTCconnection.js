58ddf535054b4499ce733e347d03b622233d3c34
Amit Kulkarni <kulkarniap123@gmail.com>
2024-07-24 17:29:49 +0530

    Update RTCconnection.js

--- frontend/src/rtc/RTCconnection.js
+++ frontend/src/rtc/RTCconnection.js
@@ -1,7 +1,10 @@
 const configuration = {
     iceServers: [
         {
-            urls: "stun:stun.stunprotocol.org"
+            urls: [
+                "stun:stun.l.google.com:19302",
+                "stun:global.stun.twilio.com:3478"
+            ]
         }
     ]
 };
