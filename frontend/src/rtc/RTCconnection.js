5530f540db70171fbf135edc4aa4f923c8ab8357
Amit Kulkarni <kulkarniap123@gmail.com>
2024-08-11 21:52:46 +0530

    added stun alternate stun servers

--- frontend/src/rtc/RTCconnection.js
+++ frontend/src/rtc/RTCconnection.js
@@ -3,7 +3,11 @@ const configuration = {
         {
             urls: [
                 "stun:stun.l.google.com:19302",
-                "stun:global.stun.twilio.com:3478"
+                "stun:global.stun.twilio.com:3478",
+                "stun:stun1.l.google.com:19302",
+                "stun:stun2.l.google.com:19302",
+                "stun:stun3.l.google.com:19302",
+                "stun:stun4.l.google.com:19302",
             ]
         }
     ]
